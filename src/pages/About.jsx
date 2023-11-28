27d4c2c8f05f4ebc6b7f1986d3aa05f2a9acfa5c
Cody Apolinar <cnapolinar@cpp.edu>
2023-11-28 12:22:55 -0800

    added css and content to about and contact pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './css/AboutUs.css';
 const About = () => {
   return (
-    <div>About</div>
+	
+    <div>
+		<h1>About</h1>
+		<p>This project was made by Ethan Jacinto, Cody Apolinar, Elvin Nguyen, and James Salac  <br /><br />
+
+			We are all Cal Poly Pomona Students. This project was created using topics learned
+			in CS2250. Some of these include React, CSS, JS, HTML, and many more. 
+		</p>
+	</div>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import './css/Contact.css';
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+		<h1>Contact</h1>
+		<p>We aren't putting phone numbers here so if something isn't working that's too bad</p>
+	</div>
   )
 }
 
