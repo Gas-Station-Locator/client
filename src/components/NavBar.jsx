8d5e94c574b8cf503a58bf0ee65baef275625f90
Ethan <ethankylejacinto@gmail.com>
2023-11-28 14:18:06 -0800

    feat: navbar styling + images + input field

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './css/NavBar.css'
+import Image76 from '../images/76.png'
+import ImageChevron from '../images/chevron.png'
+import ImageMobil from '../images/mobil.png'
 
 const NavBar = () => {
     return (
         <nav className="nav-container">
+            <div id="nav-images">
+                <img src={Image76} alt="76 Logo" className="logos" />
+                <img src={ImageMobil} alt="Mobil Logo" className="logos" />
+                <img src={ImageChevron} alt="Chevron Logo" className="logos" />
+            </div>
             <Link to="/Gas-Station-Locator">Gas Station Locator</Link>
             <Link to="/About-Us">About Us</Link>
             <Link to="/Contact-Us">Contact Us</Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,74 +16,28 @@ const HomePage = () => {
     return (
         // display map from google maps api
         <APIProvider apiKey={apiKey}>
-            <div className="map-container" style={{padding: "0px", margin: "0px", height: "100vh"}}>
-                <div className="home-message">
-                    <h1>Find Gas Near You...</h1>
+            <div className="find-gas-container">
+                <h1>Find Gas Near You...</h1>
+                <div id="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Search by state, city, or zip code"
+                        id="search-input"
+                        name="searchInput"
+                    />
                 </div>
+            </div>
+            <div className="map-container" style={{padding: "0px", margin: "0px", height: "100vh"}}>
                 <Map
                     mapTypeId={mapConfig.mapTypeId}
                     width="100%" 
                     height="100%"
-                    styles={
-                        [
-                            {
-                                featureType: "landscape",
-                                elementType: "geometry",
-                                stylers: [
-                                    {
-                                        color: "#99FF33",
-                                    }
-                                ]
-                            },
-                            {
-                                featureType: "all",
-                                elementType: "labels",
-                                stylers: [
-                                    { "visibility": "off" }
-                                ]
-                            },
-                            {
-                                featureType: "road",
-                                elementType: "all",
-                                stylers: [
-                                    { "visibility": "off" }
-                                ]
-                            },
-                            {
-                                featureType: "administrative.province",
-                                elementType: "geometry",
-                                stylers: [
-                                    { "visibility": "off" }
-                                ]
-                            },
-                            {
-                                featureType: "landscape.natural.terrain",
-                                elementType: "geometry",
-                                stylers: [
-                                    { "color": "#000000" }
-                                ]
-                            },
-                            {
-                                featureType: "landscape.natural.landcover",
-                                elementType: "geometry",
-                                stylers: [
-                                    { "color": "#e8eaec" }
-                                ]
-                            },
-                            {
-                                featureType: "water",
-                                elementType: "geometry",
-                                stylers: [
-                                    { "color": "#3c4042" }
-                                ]
-                            }
-                        ]
-                    }
                     viewState={{ 
                         latitude: position.lat, 
                         longitude: position.lng, 
                         zoom: 4
                     }}
+                    styles={mapConfig.styles}
                     disableDefaultUI={true}
                     />
             </div>
@@ -93,24 +47,57 @@ const HomePage = () => {
 
 export default HomePage;
 
+const MAP_STYLES = [
+    {
+        featureType: "all",
+        elementType: "labels",
+        stylers: [{ "visibility": "off" }]
+    },
+    {
+        featureType: "road",
+        elementType: "all",
+        stylers: [{ "visibility": "off" }]
+    },
+    {
+        featureType: "administrative.province",
+        elementType: "geometry",
+        stylers: [{ "visibility": "off" }]
+    },
+    {
+        featureType: "landscape.natural.terrain",
+        elementType: "geometry",
+        stylers: [{ "color": "#000000" }]
+    },
+    {
+        featureType: "landscape.natural.landcover",
+        elementType: "geometry",
+        stylers: [{ "color": "#e8eaec" }]
+    },
+    {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ "color": "#3c4042" }]
+    }
+];
+
 const MapTypeId = {
-    HYBRID: 'hybrid',
-    ROADMAP: 'roadmap',
-    SATELLITE: 'satellite',
-    TERRAIN: 'terrain'
+    HYBRID: "hybrid",
+    ROADMAP: "roadmap",
+    SATELLITE: "satellite",
+    TERRAIN: "terrain"
 };
 
 const MAP_CONFIGS = [
     {
-        id: 'light',
-        label: 'Light',
-        mapId: '49ae42fed52588c3',
+        id: "light",
+        label: "Light",
         mapTypeId: MapTypeId.ROADMAP
+        // customize styles for light mode
     },
     {
-        id: 'dark',
-        label: 'Dark',
-        mapId: '739af084373f96fe',
-        mapTypeId: MapTypeId.ROADMAP
+        id: "dark",
+        label: "Dark",
+        mapTypeId: MapTypeId.ROADMAP,
+        styles: MAP_STYLES
     }
 ]
