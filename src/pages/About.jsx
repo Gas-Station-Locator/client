497bdb5d7c2c1c126a880ccf79b98051a55c7026
Cody Apolinar <cnapolinar@cpp.edu>
2023-11-28 21:54:07 -0800

    updated formatting

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './css/AboutUs.css';
+import React from "react";
+import "./css/AboutUs.css";
 const About = () => {
   return (
-	
     <div>
-		<h1>About</h1>
-		<p>This project was made by Ethan Jacinto, Cody Apolinar, Elvin Nguyen, and James Salac  <br /><br />
+      <h1>About</h1>
+      <p>
+        This project was made by Ethan Jacinto, Cody Apolinar, Elvin Nguyen, and
+        James Salac <br />
+        <br />
+        We are all Cal Poly Pomona Students. This project was created using
+        topics learned in CS2250. Some of these include React, CSS, JS, HTML,
+        and many more.
+      </p>
+    </div>
+  );
+};
 
-			We are all Cal Poly Pomona Students. This project was created using topics learned
-			in CS2250. Some of these include React, CSS, JS, HTML, and many more. 
-		</p>
-	</div>
-  )
-}
-
-export default About
+export default About;
