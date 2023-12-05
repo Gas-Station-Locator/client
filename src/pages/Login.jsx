90d5ef1dfe323a8663cbd8b99a543ff0ffb1b031
Cody Apolinar <cnapolinar@cpp.edu>
2023-12-05 15:12:41 -0800

    changed fonts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,14 @@
-import './css/Login.css';
-import React from 'react'
-import { Link } from 'react-router-dom'
+import "./css/Login.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function login() {
   return (
-    <div className="App">
-      <h1>
-		Welcome to the Gas Station Locator
-	  </h1>
-	  <p>
-		Login with your information below
-	  </p>
+    <div className="main">
+      <h1>Welcome to the Gas Station Locator</h1>
+      <p>Login with your information below</p>
 
-	  <form>
+      <form>
         <div className="input-group">
           <label htmlFor="name">Username:</label>
           <input type="text" id="name" />
@@ -24,8 +20,8 @@ function login() {
         <button type="submit" className="submit-btn">
           Login
         </button>
-		
-		<button type="submit" className="sign-btn">
+
+        <button type="submit" className="sign-btn">
           <Link to="/SignUp">Sign Up</Link>
         </button>
       </form>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,17 +1,13 @@
-import './css/SignUp.css';
-import React from 'react'
-import { Link } from 'react-router-dom'
+import "./css/SignUp.css";
+import React from "react";
+import { Link } from "react-router-dom";
 function login() {
   return (
     <div className="App">
-      <h1>
-		Thank you for Signing Up
-	  </h1>
-	  <p>
-		Please enter your information below
-	  </p>
+      <h1>Thank you for Signing Up</h1>
+      <p>Please enter your information below</p>
 
-	  <form>
+      <form>
         <div className="input-group">
           <label htmlFor="name">Username:</label>
           <input type="text" id="name" />
@@ -20,8 +16,8 @@ function login() {
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" />
         </div>
-		<button type="submit" className="sign-btn">
-		<Link to="/Login">Sign up with entered Info</Link>
+        <button type="submit" className="sign-btn">
+          <Link to="/Login">Sign up with entered Info</Link>
         </button>
       </form>
     </div>
