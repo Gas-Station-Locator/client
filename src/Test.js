8429ffa3f7115e8608420850da06c955b5600e3b
elvin nguyen <elvinnguyen15@gmail.com>
2023-12-05 22:49:26 -0800

    files for gas

--- src/Test.js
+++ src/Test.js
@@ -1,67 +1,59 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-function App(){
-    // const [stations, setStations] = useState([])
-    // useEffect(()=> {
-    //     axios.get('https://localhost:5000/getStations')
-    //     .then(stations => setStations(stations.data))
-    //     .catch(err => console.log(err))
-    // }, [])
+function App() {
+  // const [stations, setStations] = useState([])
+  // useEffect(()=> {
+  //     axios.get('https://localhost:5000/getStations')
+  //     .then(stations => setStations(stations.data))
+  //     .catch(err => console.log(err))
+  // }, [])
 
-    // return(
-        
-    const URL = `https://localhost:4000/getStations/results`;
+  // return(
 
-    const [stations, setStations] = useState([]);
-    async function downloadPrice(index) {
-        const commentsURL = getstations/results;
-        const response = await fetch(commentsURL);
-        const comments = await resasyncponse.json();
-        return comments;
-    }
+  const URL = `https://localhost:4000/getStations`;
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const response = await fetch('/getStations');
-        const data = await response.json();
+  const [stations, setStations] = useState([]);
+  const [gasPrices, setGasPrices] = useState([]);
 
-        setStations(data);
-        // console.log(data[0].results[0]);
-      };
-      fetchData();
-    }, []);
+  // async function downloadPrice(index) {
+  //   const commentsURL = getstations / results;
+  //   const response = await fetch(commentsURL);
+  //   const comments = await resasyncponse.json();
+  //   return comments;
+  // }
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            Name
-                        </th>
-                        <th>
-                            Address
-                        </th>
-                        <th>
-                            Price
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        stations.map((station) => {
-                            <tr>
-                                {console.log(station)}
-                                <td>{station[0]}</td>
-                                <td>{station.address}</td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/getStations");
+      const data = await response.json();
+
+      setStations(data[0].results);
+      setGasPrices(data[0].results.gasPrices);
+      // console.log(data[0].results[1].gasPrices[0].priceTag);
+      console.log(data[0].results[1]);
+      console.log(data[0]);
+    };
+    fetchData();
+  }, []);
+
+
+
+  return (
+    <div>
+      <ul style={{ listStyle: "none"} }>
+        {stations.map((item) => {
+          return (
+            <li key={item.id}>
+              Address: {item.address} ---
+              Gas Price: {item.gasPrices.priceTag}
+              {console.log(item.gasPrices.priceTag)}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
